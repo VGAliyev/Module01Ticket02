468ffc58ae8db0c520102a8dcb6ab7260d7806e0
VGAliyev <v.aliev84@yandex.ru>
2023-09-16 15:47:05 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Курсовая работа (Введение в профессию и в синтаксис языка)");
+
+        System.out.println("Билет № 2");
+
+        //  Madam, I'm Adam!
+        // А роза упала на лапу Азора
+
+
+    }
+}
